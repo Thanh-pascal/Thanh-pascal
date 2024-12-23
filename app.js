3c6916e719c2929643821f559f46fad62ad29e99
Thanh-pascal <quynhphuonglqd@gmail.com>
2024-12-23 16:18:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,73 +1,51 @@
-var progress = document.querySelector(".progress");
-var percent = document.querySelector(".percent");
-var textBox = document.querySelector(".textBox");
-var button = document.querySelector(".button");
-var textBtn = document.querySelector(".textBtn");
-var heartBox = document.querySelector(".heartBox");
-var cursor = document.querySelector(".buttonCLick img"); 
-var heartItem1 = document.querySelector(".heartItem.item1")
-var heartItem2 = document.querySelector(".heartItem.item2")
-var heartItem3 = document.querySelector(".heartItem.item3")
-var count = 0;
-var percentWidth = 0;
-var heartLeft = -3.2;
-var progressLoad = setInterval(progressInterva,100)
+  "object" != typeof window.CP && (window.CP = {}),
+  (window.CP.PenTimer = {
+    programNoLongerBeingMonitored: !1,
+    timeOfFirstCallToShouldStopLoop: 0,
+    _loopExits: {},
+    _loopTimers: {},
+    START_MONITORING_AFTER: 2e3,
+    STOP_ALL_MONITORING_TIMEOUT: 5e3,
+    MAX_TIME_IN_LOOP_WO_EXIT: 2200,
+    exitedLoop: function (E) {
+      this._loopExits[E] = !0;
+    },
+    shouldStopLoop: function (E) {
+      if (this.programKilledSoStopMonitoring) return !0;
+      if (this.programNoLongerBeingMonitored) return !1;
+      if (this._loopExits[E]) return !1;
+      var _ = this._getTime();
+      if (0 === this.timeOfFirstCallToShouldStopLoop)
+        return (this.timeOfFirstCallToShouldStopLoop = _), !1;
+      var o = _ - this.timeOfFirstCallToShouldStopLoop;
+      if (o < this.START_MONITORING_AFTER) return !1;
+      if (o > this.STOP_ALL_MONITORING_TIMEOUT)
+        return (this.programNoLongerBeingMonitored = !0), !1;
+      try {
+        this._checkOnInfiniteLoop(E, _);
+      } catch {
+        return (
+          this._sendErrorMessageToEditor(),
+          (this.programKilledSoStopMonitoring = !0),
+          !0
+        );
+      }
+      return !1;
+    },
 
-button.addEventListener("click", function(){
-    button.style.transform = "scale(0.8)";
-    setTimeout(()=>{
-        button.style.transform = "scale(1)";
-        window.location.href = "./love/love.html"
-    },200)
-})
-
-function progressInterva(){
-    if(count == 100 && percentWidth == 100){
-        clearInterval(progressLoad)
-        percent.textContent = "Quá trình hoàn tất:v";
-        percent.style.letterSpacing = "1px";
-        textBox.style.transform = "scale(1.3)"
-        heartItem3.style.animation = "1s heartScale forwards"
-        setTimeout(()=>{
-            textBox.style.transform = "scale(0)"
-        },400)
-        setTimeout(()=>{
-            textBox.style.opacity = "0"
-        },600)
-        setTimeout(()=>{
-            button.style.transform= "scale(1)";
-        },800);
-        setTimeout(()=>{
-            button.style.background = "rgb(244,118,121)"
-            button.style.width = "130px";
-            button.style.borderRadius = "20px"
-        },1500)
-        setTimeout(()=>{
-            button.style.height = "40px";
-        },2000)
-        setTimeout(()=>{
-            textBtn.textContent = "Click me!"
-            textBtn.style.color = "#fff"
-        },2500)
-        setTimeout(()=>{
-            cursor.style.opacity = "1";
-        },3000)
-    }
-    else{
-        if(count == 10){
-            heartItem1.style.animation = "1s heartScale forwards"
-        }
-        if(count ==46){
-            percent.style.color= "#fff"
-        }
-        if(count == 60){
-            heartItem2.style.animation = "1s heartScale forwards"
-        }
-        count += 1;
-        percentWidth += 1;
-        heartLeft += 0.968;
-        progress.style.width = percentWidth + '%'
-        percent.innerText = count + '%'
-        heartBox.style.left = heartLeft + '%'
-    }
-}
+    _checkOnInfiniteLoop: function (E, _) {
+      if (!this._loopTimers[E]) return (this._loopTimers[E] = _), !1;
+      if (_ - this._loopTimers[E] > this.MAX_TIME_IN_LOOP_WO_EXIT)
+        throw "Infinite Loop found on loop: " + E;
+    },
+    _getTime: function () {
+      return Date.now();
+    },
+  }),
+  (window.CP.shouldStopExecution = function (E) {
+    var _ = window.CP.PenTimer.shouldStopLoop(E);
+    return 
+  }),
+  (window.CP.exitedLoop = function (E) {
+    window.CP.PenTimer.exitedLoop(E);
+  });
